f66e0e438a95a24aa82516ac43a56ba01b1cffa8
Refayat Haque <refayathaque@gmail.com>
2017-04-04 23:04:16 -0400

    .

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-var x = 4 // Ruby: x = 4
-var y = 5 // Ruby: x = 5
+function greetSomeone(person){
+  var person1 = person.toLowerCase();
+  if (person1 == "refayat"){
+    console.log("Hey there Refayat!");
+}
+else {
+  console.log("Hey there other guy!")
+}
+}
 
-// Ruby: puts x + y
-console.log(x + y); // Outputs 9
-
-// Node's REPL (analogous to PRY/IRB in Ruby) is
-// just 'node' in the terminal, to exit out do
-// '.exit'
+greetSomeone("Refayat")
